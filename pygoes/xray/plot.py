5d889d7f5f233993369e68b400195d2d76e10c73
James Whinfrey <james@conceptric.co.uk>
2013-03-20 16:13:58 +0000

    Simplifies the GOES data plotting function

--- pygoes/xray/plot.py
+++ pygoes/xray/plot.py
@@ -2,13 +2,12 @@ import atpy
 import numpy as np
 import matplotlib.dates as pld
 
-def plot_goes(plotaxis, goes, datemin, datemax):
-    ''' Plots the GOES X-Ray data for a defined time range on the specified axis
-        plotaxis: pyplot axis object
-        goes    : formatted GOES datetime and xray flux data table
-        datemin : Numpy datetime64 of the start time
-        datemax : Numpy datetime64 of the end time
+def plot_goes(paxis, gdata):
+    ''' Plots the GOES X-Ray data on the specified axis.
+    
+        plotaxis : pyplot axis object
+        gdata    : an instance of GoesFile
     '''
-    pgoes = goes.where((goes.datetime.astype(np.datetime64) >= datemin) & (goes.datetime.astype(np.datetime64) <= datemax))
-    plotaxis.plot_date(pld.date2num(pgoes.datetime.astype(np.datetime64).astype(object)), pgoes['0.1-0.8 nanometer (W/m2)'], label='goes-15', fmt='--') 
-    plotaxis.set_ylabel("GOES-15 0.1 - 0.8 nanometer (W/m2)")
+    paxis.plot_date(pld.date2num(gdata.datetime.astype(np.datetime64).astype(object)), 
+        gdata['0.1-0.8 nanometer (W/m2)'], label='GOES-15', fmt='--') 
+    paxis.set_ylabel("GOES-15 0.1 - 0.8 nanometer (W/m2)")
