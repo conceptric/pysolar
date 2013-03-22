d2a852953fb74c9daad510d17e56281a8da27209
James Whinfrey <james@conceptric.co.uk>
2013-03-22 13:24:33 +0000

    Minor cleanup and refactor of the GoesFile class test suite

--- pygoes/tests/xray_data_test.py
+++ pygoes/tests/xray_data_test.py
@@ -83,73 +83,82 @@ class TestDataSetGetDateRanges(unittest.TestCase):
 
 class TestGoesFile(unittest.TestCase):
     """ 
-    Test the class that imports GOES-15 X-Ray data files 
+    Test the GoesFile class instantiates and handles errors
     """
-    def setUp(self):
-        test_root = os.path.dirname(__file__)
-        test_data = os.path.join(test_root, '20130227_Gp_xr_5m.txt')
-        self.goesfile = GoesFile(test_data)
-
     def test_requires_path(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(TypeError):
             GoesFile()
 
-    def test_incorrect_path(self):
-        with self.assertRaises(Exception):
-            GoesFile(os.path.join(test_root, 'wrong_filename.txt'))
+    def test_incorrect_path_filename(self):
+        with self.assertRaises(IOError):
+            GoesFile(os.path.join(TEST_ROOT, 'wrong_filename.txt'))
+
+    def test_blank_path_filename(self):
+        with self.assertRaises(IOError):
+            GoesFile(os.path.join(TEST_ROOT, ''))
+
+    def test_invalid_path_type(self):
+        with self.assertRaises(IOError):
+            GoesFile('/invalid')
+
+    def test_incomplete_path(self):
+        with self.assertRaises(IOError):
+            GoesFile(TEST_ROOT)
 
     def test_exists(self):
-        self.assert_(self.goesfile)
+        test_file = os.path.join(TEST_ROOT, '20130227_Gp_xr_5m.txt')
+        self.assert_(GoesFile(test_file))
+
+
+class TestGoesFileFormatting(unittest.TestCase):
+    """ 
+    Test the data the GoesFile class imports, how it 
+    formats it, and accesses the results.
+    """
+    def setUp(self):
+        self.goes = GoesFile(os.path.join(TEST_ROOT, FILENAMES[0]))
+        self.glength = 288
 
     def test_length(self):
-        expected = 288
-        actual = len(self.goesfile.table)
-        self.assertEqual(actual, expected)
+        self.assertEqual(self.glength, len(self.goes.table))
 
     def test_original_columns(self):
-        expected = ['col1', 'col2', 'col3', 
-            'col4', 'col5', 'col6', 'col7', 'col8']
-        actual = sorted(self.goesfile.columns.keys())
-        self.assertEqual(actual, expected)
+        expected = ['col1', 'col2', 'col3', 'col4', 
+                    'col5', 'col6', 'col7', 'col8']
+        actual = sorted(self.goes.columns.keys())
+        self.assertEqual(expected, actual)
         
     def test_new_columns(self):
-        expected = ('year', 
-                    'month',
-                    'day',
-                    'time',
-                    'JD days',
-                    'JD secs',
+        expected = ('year', 'month', 'day', 'time',
+                    'JD days', 'JD secs',
                     '0.05-0.4 nanometer (W/m2)',
                     '0.1-0.8 nanometer (W/m2)',
-                    'datetime',
-                    'Modified JD')
-        actual = self.goesfile.table.names
-        self.assertEqual(actual, expected)
+                    'datetime', 'Modified JD')
+        actual = self.goes.table.names
+        self.assertEqual(expected, actual)
 
     def test_first_datetime(self):
-        expected = "2013-02-27 00:00"
-        actual = self.goesfile.table.datetime[0]
-        self.assertEqual(actual, expected)
+        actual = self.goes.table.datetime[0]
+        self.assertEqual("2013-02-27 00:00", actual)
         
     def test_last_datetime(self):
-        expected = "2013-02-27 23:55"
-        actual = self.goesfile.table.datetime[287]
-        self.assertEqual(actual, expected)
+        actual = self.goes.table.datetime[self.glength - 1]
+        self.assertEqual("2013-02-27 23:55", actual)
         
     def test_get_date_range(self):
         start   = "2013-02-27 00:30"
         end     = "2013-02-27 01:00"
-        actual = self.goesfile.get_date_range(start, end)
+        actual = self.goes.get_date_range(start, end)
         self.assertIsInstance(actual, Table)
         self.assertEqual(actual.datetime[0], start)
         self.assertEqual(len(actual), 7)
         self.assertEqual(actual.datetime[6], end)
 
     def test_first_modified_jd(self):
-        actual = self.goesfile.table['Modified JD'][0]
-        self.assertEqual(actual, 56350.00000)
+        actual = self.goes.table['Modified JD'][0]
+        self.assertEqual(56350.00000, actual)
         
     def test_last_modified_jd(self):
-        actual = self.goesfile.table['Modified JD'][287]
-        self.assertEqual(actual, 56350.99653)
+        actual = self.goes.table['Modified JD'][287]
+        self.assertEqual(56350.99653, actual)
         
