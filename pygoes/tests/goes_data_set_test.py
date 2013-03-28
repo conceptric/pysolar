b9475bed58c15271b1932ead1caa0436e8e640d6
James Whinfrey <james@conceptric.co.uk>
2013-03-28 15:30:19 +0000

    Extracts the GoesDataSet test suite into a new file

---
+++ pygoes/tests/goes_data_set_test.py
@@ -0,0 +1,82 @@
+import unittest
+import os
+from atpy.basetable import Table
+from pygoes.xray.data import GoesDataSet
+
+TEST_ROOT = os.path.dirname(__file__)
+FILENAMES =  ['20130227_Gp_xr_5m.txt', '20130228_Gp_xr_5m.txt']
+
+class TestDataSet(unittest.TestCase):
+    """ 
+    Test that the DataSet class exists and has
+    the correct attributes. 
+    """
+    def setUp(self):
+        self.dataset = GoesDataSet()
+            
+    def test_exists(self):
+        self.assert_(self.dataset)
+
+    def test_has_empty_datafiles_attribute(self):
+        self.assertEquals(self.dataset.datafiles, [])
+
+class TestDataSetCompile(unittest.TestCase):
+    """ 
+    Test that the DataSet class can import multiple 
+    GOES-15 X-Ray data files.
+    """
+    def setUp(self):
+        self.dataset = GoesDataSet()
+
+    def test_ignores_empty_file_list(self):
+        self.dataset.compile(TEST_ROOT, [])
+        self.assertEquals(0, len(self.dataset.datafiles))
+
+    def test_ignores_incorrect_filename_in_list(self):
+        self.dataset.compile(TEST_ROOT, ['wrong_name.txt'])
+        self.assertEquals(0, len(self.dataset.datafiles))        
+
+    def test_add_file_to_dataset(self):
+        self.dataset.compile(TEST_ROOT, [FILENAMES[0]])
+        actual = len(self.dataset.datafiles)
+        self.assertEquals(1, actual)
+
+    def test_add_two_files_to_dataset(self):
+        self.dataset.compile(TEST_ROOT, FILENAMES)
+        actual = len(self.dataset.datafiles)
+        self.assertEquals(2, actual)
+
+class TestDataSetGetDateRanges(unittest.TestCase):
+    """ 
+    Test the methods for working with date ranges
+    that spread over multiple files.
+    """    
+    def setUp(self):
+        self.dataset = GoesDataSet()
+        self.dataset.compile(TEST_ROOT, FILENAMES)
+        self.start   = "2013-02-27 23:50"
+        self.end     = "2013-02-28 00:10"
+        self.records = 5
+    
+    def test_returns_a_table(self):
+        actual = self.dataset.get_date_range(self.start, self.end)
+        self.assertIsInstance(actual, Table)
+
+    def test_correct_number_of_records(self):
+        actual = self.dataset.get_date_range(self.start, self.end)
+        self.assertEqual(len(actual), self.records)
+    
+    def test_files_in_date_order(self):
+        actual = self.dataset.get_date_range(self.start, self.end)
+        self.assertEqual(actual.datetime[0], self.start)
+        self.assertEqual(actual.datetime[self.records - 1], self.end)
+
+    def test_files_not_in_date_order(self):
+        reversed_dataset = GoesDataSet()
+        reversed_dataset.compile(TEST_ROOT, reversed(FILENAMES))
+        actual = reversed_dataset.get_date_range(self.start, self.end)
+        self.assertEqual(actual.datetime[0], self.start)
+        self.assertEqual(actual.datetime[self.records - 1], self.end)
+
+
+
