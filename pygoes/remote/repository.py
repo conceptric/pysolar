b776035e3883158e48227c7e2712bd326d6ef651
James Whinfrey <james@conceptric.co.uk>
2013-04-01 18:44:46 +0100

    Removes the open method from the public API. Tests the error handling.

--- pygoes/remote/repository.py
+++ pygoes/remote/repository.py
@@ -1,4 +1,6 @@
 import urllib2
+from contextlib import closing
+
 from pygoes.utils.errors import MissingFileError
 
 class RemoteManager:
@@ -9,17 +11,16 @@ class RemoteManager:
         urllib2.urlopen(urllib2.Request(settings.source))
         self.url = settings.source
 
-    def open(self, filename):
+    def read(self, filename):
+        with closing(self.__open(filename)) as remote:
+            content = remote.read()        
+            return content
+
+    def __open(self, filename):
         try:
             return urllib2.urlopen(self.url + '/' + filename)
         except urllib2.HTTPError as ex:
-            self.__handle_http_error(ex, filename)            
-
-    def read(self, filename):
-        remote = self.open(filename)
-        content = remote.read()        
-        remote.close()        
-        return content
+            self.__handle_http_error(ex, filename)
 
     def __handle_http_error(self, http_error, filename):
         if http_error.code == 404:
--- pygoes/tests/remote_manager_tests.py
+++ pygoes/tests/remote_manager_tests.py
@@ -19,22 +19,19 @@ class TestRemoteManager(unittest.TestCase):
     def test_requires_a_source_path_in_settings(self):
         self.assertTrue(self.remote)
 
-    def test_can_open_remote_files(self):
-        self.assertTrue(self.remote.open("Gp_xr_5m.txt"))
-
     def test_raises_error_with_local_path_for_url(self):
         with self.assertRaises(ValueError):
             RemoteManager(FakeSettings(FIXTURES))
     
-    def test_raises_helpful_error_if_file_does_not_exist(self):
-        filename = "missing.txt"
-        message = "%s is missing in the remote location." % filename
-        try:
-            self.remote.open("missing.txt")
-            self.assertFail()
-        except MissingFileError as ex:
-            self.assertEquals(ex.message, message)        
-
     def test_read_existing_remote_file(self):
         first_line = self.remote.read("Gp_xr_5m.txt").split('\n')[0]        
         self.assertEqual(first_line, ":Data_list: Gp_xr_5m.txt")
+                
+    def test_reading_missing_file_raises_helpful_error(self):
+        filename = "missing.txt"
+        message = "%s is missing in the remote location." % filename
+        try:
+            self.remote.read(filename)
+            self.assertFail()
+        except MissingFileError as ex:
+            self.assertEquals(ex.message, message)        
