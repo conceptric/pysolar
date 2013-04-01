478419204846d64ec911bfbf1008ab495a4d359a
James Whinfrey <james@conceptric.co.uk>
2013-04-01 18:26:48 +0100

    Adds tests for the RemoteManager class. The downloader keeps getting simpler

--- pygoes/remote/file_downloader.py
+++ pygoes/remote/file_downloader.py
@@ -9,16 +9,7 @@ class NamedFileDownloader:
         self.remote = remote_manager
         self.cache = cache_manager
 
-    def __handle_http_error(self, http_error, filename):
-        if http_error.code == 404:
-            raise MissingFileError("%s is missing in the remote location." % filename)
-        else:
-            raise        
-            
     def download(self, filename):
         if not self.cache.file_exists(filename):
-            try:
-                remote = self.remote.read(filename)
-                self.cache.write_file(filename, remote)
-            except urllib2.HTTPError as ex:
-                self.__handle_http_error(ex, filename)
+            remote = self.remote.read(filename)
+            self.cache.write_file(filename, remote)
--- pygoes/remote/repository.py
+++ pygoes/remote/repository.py
@@ -1,18 +1,30 @@
 import urllib2
+from pygoes.utils.errors import MissingFileError
 
 class RemoteManager:
     """
     Class that represents a remote repository of data files.
     """
     def __init__(self, settings):
+        urllib2.urlopen(urllib2.Request(settings.source))
         self.url = settings.source
 
     def open(self, filename):
-        return urllib2.urlopen(self.url + '/' + filename)
-    
+        try:
+            return urllib2.urlopen(self.url + '/' + filename)
+        except urllib2.HTTPError as ex:
+            self.__handle_http_error(ex, filename)            
+
     def read(self, filename):
         remote = self.open(filename)
         content = remote.read()        
         remote.close()        
         return content
+
+    def __handle_http_error(self, http_error, filename):
+        if http_error.code == 404:
+            raise MissingFileError("%s is missing in the remote location." % 
+                filename)
+        else:
+            raise        
 
