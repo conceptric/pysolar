3d1175dfe85390bdd05034fad3b7c1ede1ecfb18
James Whinfrey <james@conceptric.co.uk>
2013-04-01 15:01:53 +0100

    Extracts the cache functionality into a new class. The downloader should not be in charge of the locally cached files

--- pygoes/remote/file_downloader.py
+++ pygoes/remote/file_downloader.py
@@ -2,32 +2,41 @@ import os
 import urllib2
 from pygoes.utils.errors import MissingFileError
 
+class CacheManager:
+    """
+    Class to manage the local data file cache.
+    """
+    def __init__(self, settings):
+        self.path = settings.cache
+
+    def file_exists(self, filename):
+        return os.path.exists(os.path.join(self.path, filename))
+        
+    def write_file(self, filename, data):
+        cache = open(os.path.join(self.path, filename), 'w')
+        cache.write(data)
+        cache.close()
+            
+
 class NamedFileDownloader:
     """ 
     Class that downloads the requested file 
     """
     def __init__(self, settings):
         self.settings = settings
+        self.cache = CacheManager(settings)
 
     def __open_remote(self, filename):
         return urllib2.urlopen(self.settings.source + '/' + filename)
 
-    def __path_to_cached_file(self, filename):
-        return os.path.join(self.settings.cache, filename)
-
     def already_cached(self, filename):
-        return os.path.exists(self.__path_to_cached_file(filename))
+        return self.cache.file_exists(filename)
     
     def read_remote(self, filename):
         remote = self.__open_remote(filename)
         content = remote.read()        
         remote.close()        
         return content
-
-    def write_cached(self, filename, content):
-        cache = open(self.__path_to_cached_file(filename), 'w')
-        cache.write(content)
-        cache.close()
 
     def __handle_http_error(self, http_error, filename):
         if http_error.code == 404:
@@ -36,9 +45,9 @@ class NamedFileDownloader:
             raise        
             
     def download(self, filename):
-        if not self.already_cached(filename):
+        if not self.cache.file_exists(filename):
             try:
                 remote = self.read_remote(filename)
-                self.write_cached(filename, remote)
+                self.cache.write_file(filename, remote)
             except urllib2.HTTPError as ex:
                 self.__handle_http_error(ex, filename)
