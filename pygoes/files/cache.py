2b7880df7f1773bb1c6315f8335975104324d92d
James Whinfrey <james@conceptric.co.uk>
2013-04-03 16:29:00 +0100

    Refactors the CacheManager test suite and improves the code documentation

--- pygoes/files/cache.py
+++ pygoes/files/cache.py
@@ -4,13 +4,31 @@ class CacheManager:
     """
     Class to manage the local data file cache.
     """
-    def __init__(self, settings):
-        self.path = settings.cache
+    def __init__(self, config):
+        '''
+        Checks if the cache path provided in the configuration 
+        exists. Raises a ValueError if not.
+        '''
+        if os.path.exists(config.cache):
+            self.path = config.cache
+        else:
+            raise ValueError("Cache directory does not exist.")
 
     def file_exists(self, filename):
+        ''' 
+        Checks whether a file named with the string filename 
+        exists in the local cache. 
+        Returns True if it exists or False if not.
+        '''
         return os.path.exists(os.path.join(self.path, filename))
         
     def write_file(self, filename, data):
+        '''
+        Write a file to the local cache only if it does not 
+        already exists.
+        filename: string defining the name of the file.
+        data    : string for the contents of the new file.
+        '''
         if not self.file_exists(filename):
             cache = open(os.path.join(self.path, filename), 'w')
             cache.write(data)
--- pygoes/files/tests/cache_manager_test.py
+++ pygoes/files/tests/cache_manager_test.py
@@ -1,54 +1,72 @@
 import unittest
+
 from remote_test_config import *
 from pygoes.files.cache import CacheManager
 
-class FakeSettings:
-    cache = FIXTURES
+class FakeConfig:
+    def __init__(self, path=FIXTURES):
+        self.cache = path
+
 
 class TestCacheManager(unittest.TestCase):
     """
-    Test the class that is responsible for managing the local cache
-    of data files
+    Test the arguments for a CacheManager.
     """
-    def setUp(self):
-        self.cache = CacheManager(FakeSettings())
+    def test_requires_a_configuration_argument(self):
+        self.assertTrue(CacheManager(FakeConfig()))
         
-    def test_requires_a_cache_path_in_settings(self):
-        self.assertTrue(self.cache)
+    def test_raises_error_without_configuration_argument(self):
+        with self.assertRaises(TypeError):
+            CacheManager()
     
+    def test_raises_error_a_url_for_local_path(self):
+        with self.assertRaises(ValueError):
+            CacheManager(FakeConfig(REMOTE))
+
+
+class TestFileManagement(unittest.TestCase):
+    """
+    Test what the CacheManager knows about the Files
+    """    
+    def setUp(self):
+        self.cache = CacheManager(FakeConfig())
+
     def test_knows_if_a_file_is_already_cached(self):
         self.assertTrue(self.cache.file_exists("existing_file.txt"))
         
     def test_knows_if_a_file_is_not_cached(self):
         self.assertFalse(self.cache.file_exists("missing.txt"))
 
+
 class TestWriteFile(unittest.TestCase):
     """
     Test creating and writing to cached text files
     """
     def setUp(self):
-        self.cache = CacheManager(FakeSettings())
+        self.cache = CacheManager(FakeConfig())
         self.filename = "writing_test.txt"
         self.content = "Original"
-        self.file_path = os.path.join(FIXTURES, self.filename)
         self.cache.write_file(self.filename, self.content)
         
     def tearDown(self):
-        if os.path.exists(self.file_path):
-            os.remove(self.file_path)
+        os.remove(get_cached_path(self.filename))
     
     def actual(self):
-        return open(self.file_path, 'r').read()
+        return open(get_cached_path(self.filename), 'r').read()
             
     def test_writing_to_new_file(self):
-        self.assertTrue(self.cache.file_exists(self.filename))
+        self.assertTrue(cached_file_exists(self.filename))
         self.assertEquals(self.actual(), self.content)
 
     def test_existing_file_cannot_be_overwritten(self):
         self.cache.write_file(self.filename, "Overwritten")
-        self.assertTrue(self.cache.file_exists(self.filename))
+        self.assertTrue(cached_file_exists(self.filename))
         self.assertEquals(self.actual(), self.content)
         
     def test_raises_exception_with_invalid_path(self):
         with self.assertRaises(IOError):
             self.cache.write_file("wrong/path.txt", "Error")
+
+
+if __name__ == '__main__':
+    unittest.main()
