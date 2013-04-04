d27b54daeeef9921e231145b6b6dc2c2d0bd707c
James Whinfrey <james@conceptric.co.uk>
2013-04-04 16:50:48 +0100

    Moves the new date utilities to the utils module

--- pygoes/files/tests/utils_test.py
+++ pygoes/files/tests/utils_test.py
@@ -1,16 +1,6 @@
 import unittest
-from datetime import date, timedelta
 
-def get_range_of_dates(start=None, finish=None):
-    if start and finish:
-        offset = range(0, ((finish - start).days + 1))
-        return [ start + timedelta(days=x) for x in offset ]
-    else:
-        return []
-
-def get_date_range_strings(start, finish, format='%Y%m%d'):
-    date_range = get_range_of_dates(start, finish)
-    return [ adate.strftime(format) for adate in date_range ]
+from pygoes.files.utils import *
 
 class TestGetDateRange(unittest.TestCase):
     """
--- pygoes/files/utils.py
+++ pygoes/files/utils.py
@@ -1,6 +1,20 @@
+from datetime import date, timedelta
+
 '''
-Module for custom errors relevant to the files package.
+Module for utilites and custom errors for the files package.
 '''
+def get_range_of_dates(start=None, finish=None):
+    if start and finish:
+        offset = range(0, ((finish - start).days + 1))
+        return [ start + timedelta(days=x) for x in offset ]
+    else:
+        return []
+
+def get_date_range_strings(start, finish, format='%Y%m%d'):
+    date_range = get_range_of_dates(start, finish)
+    return [ adate.strftime(format) for adate in date_range ]
+
 class MissingFileError(Exception):
     """ An error for when a file could not be found. """
     pass
+
