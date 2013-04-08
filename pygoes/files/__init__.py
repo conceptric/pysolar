a33cc6ccb7e93c9b25bfdebfd17563cc205201cd
James Whinfrey <james@conceptric.co.uk>
2013-04-08 12:21:39 +0100

    Moves the CacheManager class into the Repository module

--- pygoes/files/__init__.py
+++ pygoes/files/__init__.py
@@ -3,6 +3,5 @@ Package to managing all the data files used in the PyGoes project.
 '''
 from configuration import *
 from utils import *
-from cache import *
 from downloader import *
 from repository import *
