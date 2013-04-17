fe7d4daf2034f9c822e82680eabd716ce782a7b5
James Whinfrey <james@conceptric.co.uk>
2013-04-17 14:06:08 +0100

    Extract the GoesFile subclass test suite to a new file and rename the original to reflect the contained class name

---
+++ pysolar/data/tests/goes_file_subclass_test.py
@@ -0,0 +1,46 @@
+import unittest
+
+from data_test_config import *
+from pysolar.data import *
+
+
+class TestAnXrayGoesFile(unittest.TestCase):
+    """ 
+    Test that the GoesFile imports the XRay data properly.
+    """
+    def setUp(self):
+        self.goes = XrayGoesFile(os.path.join(FIXTURES, '20130227_Gp_xr_5m.txt'))
+        self.glength = 288
+
+    def test_file_length(self):
+        self.assertEqual(self.glength, self.goes.size())
+        
+    def test_xray_data_column_names_exist(self):
+        expected = ('0.05-0.4 nanometer (W/m2)',
+                    '0.1-0.8 nanometer (W/m2)')
+        actual = self.goes.names()
+        for item in expected:
+            self.assertTrue(item in actual, "'%s' is missing" % (item))
+
+
+class TestAMagneticGoesFile(unittest.TestCase):
+    """ 
+    Test that the GoesFile imports the Magnetometry data properly.
+    """
+    def setUp(self):
+        self.goes = MagneticGoesFile(os.path.join(FIXTURES, '20130322_Gp_mag_1m.txt'))
+        self.glength = 1440
+
+    def test_file_length(self):
+        self.assertEqual(self.glength, self.goes.size())
+        
+    def test_magnetic_data_column_names_exist(self):
+        expected = ('Hp (nT)', 'He (nT)', 'Hn (nT)',
+                    'Total Field (nT)')
+        actual = self.goes.names()
+        for item in expected:
+            self.assertTrue(item in actual, "'%s' is missing" % (item))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- pysolar/data/tests/goes_file_test.py
+++ pysolar/data/tests/goes_file_test.py
@@ -4,7 +4,6 @@ from data_test_config import *
 from pysolar.data import *
 
 XRAY =  '20130227_Gp_xr_5m.txt'
-MAG =   '20130322_Gp_mag_1m.txt'
 
 class TestGoesFile(unittest.TestCase):
     """ 
@@ -80,44 +79,6 @@ class TestGoesFileGetDateRange(unittest.TestCase):
     def test_last_modified_jd(self):
         actual = self.goes.table.ModifiedJD[287]
         self.assertEqual(56350.99653, actual)
-
-
-class TestAnXrayGoesFile(unittest.TestCase):
-    """ 
-    Test that the GoesFile imports the XRay data properly.
-    """
-    def setUp(self):
-        self.goes = XrayGoesFile(os.path.join(FIXTURES, XRAY))
-        self.glength = 288
-
-    def test_file_length(self):
-        self.assertEqual(self.glength, self.goes.size())
-        
-    def test_xray_data_column_names_exist(self):
-        expected = ('0.05-0.4 nanometer (W/m2)',
-                    '0.1-0.8 nanometer (W/m2)')
-        actual = self.goes.names()
-        for item in expected:
-            self.assertTrue(item in actual, "'%s' is missing" % (item))
-
-
-class TestAMagneticGoesFile(unittest.TestCase):
-    """ 
-    Test that the GoesFile imports the Magnetometry data properly.
-    """
-    def setUp(self):
-        self.goes = MagneticGoesFile(os.path.join(FIXTURES, MAG))
-        self.glength = 1440
-
-    def test_file_length(self):
-        self.assertEqual(self.glength, self.goes.size())
-        
-    def test_magnetic_data_column_names_exist(self):
-        expected = ('Hp (nT)', 'He (nT)', 'Hn (nT)',
-                    'Total Field (nT)')
-        actual = self.goes.names()
-        for item in expected:
-            self.assertTrue(item in actual, "'%s' is missing" % (item))
 
 
 if __name__ == '__main__':
